f28d4675bc439965056f6e9b3cb02ad205253cf9
Tony Theodoropoulos <theodot@outlook.com>
2020-11-20 15:22:05 -0500

    Updated to include questions from repl.it

--- MathOperatorsTemplate/Form1.Designer.cs
+++ MathOperatorsTemplate/Form1.Designer.cs
@@ -37,19 +37,17 @@
             this.outputLabel.BackColor = System.Drawing.Color.Black;
             this.outputLabel.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.outputLabel.ForeColor = System.Drawing.Color.Lime;
-            this.outputLabel.Location = new System.Drawing.Point(22, 24);
-            this.outputLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.outputLabel.Location = new System.Drawing.Point(12, 13);
             this.outputLabel.Name = "outputLabel";
-            this.outputLabel.Size = new System.Drawing.Size(945, 410);
+            this.outputLabel.Size = new System.Drawing.Size(326, 335);
             this.outputLabel.TabIndex = 0;
             this.outputLabel.Text = "Welcome...";
             // 
             // runButton
             // 
-            this.runButton.Location = new System.Drawing.Point(24, 456);
-            this.runButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.runButton.Location = new System.Drawing.Point(236, 369);
             this.runButton.Name = "runButton";
-            this.runButton.Size = new System.Drawing.Size(187, 78);
+            this.runButton.Size = new System.Drawing.Size(102, 42);
             this.runButton.TabIndex = 1;
             this.runButton.Text = "Run";
             this.runButton.UseVisualStyleBackColor = true;
@@ -57,12 +55,11 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(982, 574);
+            this.ClientSize = new System.Drawing.Size(351, 427);
             this.Controls.Add(this.runButton);
             this.Controls.Add(this.outputLabel);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Math Operators";
--- MathOperatorsTemplate/Form1.cs
+++ MathOperatorsTemplate/Form1.cs
@@ -24,17 +24,74 @@ namespace MathOperatorsTemplate
 
         private void runButton_Click(object sender, EventArgs e)
         {
-            // Variable Declarations
-            double numberOne, numberTwo, numberThree;
-            double sum, difference, product, quotient;
+            //1. Declare a string variable, store your name in it, and display it
 
-            //Variable Assignment
-            numberOne = 30;
-            numberTwo = 5.17;
-            numberThree = 2.11;
 
-            //Calculation and output lines
-            outputLabel.Text = "First Number: " + numberOne + "  Second Number:  " + numberTwo + "  Third Number: " + numberThree + "\n\n";
+            //2. Declare an integer variable, store your age in it, and display it
+
+
+            //3. Declare a double variale, store a decimal number, and display it
+
+
+            //4. Combine a text with a variable to display "Hello" followed by your name
+
+
+            /* 5. Simple arithmetic is done by using the +, -, * and / operators, 
+               (plus, minus, times and divide). Results of mathematical expressions 
+               should be stored in a variable. 
+
+               Uncomment and un the code below to see what the result is. Notice that 
+               the output does not show the values of x and y, but instead the words x and y. 
+
+               Change the code to subtract y from x, and output the following:
+
+               x - y = 2
+            */
+
+            //int x = 6;
+            //int y = 4;
+            //int answer = x + y;
+
+            //outputLabel.Text += $"\n\nx + y = {answer}";
+
+
+            /* 6. Create a program that calculates how much money an employee has made based
+               on their pay rate and the number of hours worked.
+
+
+               - Create a variable to hold the rate of pay, (payRate), and set it to 15.25
+               - Create a variable to hold the hours worked, (hoursWorked), and set it to 8
+               - Create a variable to hold the total pay, (totalPay)
+               - Calculate the total pay
+               - Display the results to the screen as per the output below.
+
+               Rate of Pay: 15.25
+               Hours Worked: 8
+               Total Pay: 122
+            */
+
+
+            /* 7. Create a program which calculates the tax amount for meal at a restaurant.
+                   
+              - Create a variable to hold the bill amount, (billAmount), of 22.50
+              - Create a variable to hold the tax rate, (taxRate), of 0.13
+              - Create a variable to hold the tax amount, (taxAmount)
+              - Calculate the tax, (billAmount * taxRate) and store it in taxAmount 
+
+                Bill Amount: 22.50
+                13% Tax: 2.925             
+            */
+
+
+            // 8. Increment your age variable by 1 using ++ and display it
+
+
+            // 9. Decrement the value created above by 1 using -- and dislay it          
+
+
+            // 10. Format the output of question 7 above to show dollar signs and 2 decimal places
+
+
         }
     }
 }
