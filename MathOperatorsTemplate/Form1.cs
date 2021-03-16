41bbf638efc269ab926f0d042b71d6c4050c2303
Tony Theodoropoulos <theodot@outlook.com>
2021-03-16 14:15:25 -0400

    updated to /// comments

--- MathOperatorsTemplate/Form1.cs
+++ MathOperatorsTemplate/Form1.cs
@@ -24,31 +24,30 @@ namespace MathOperatorsTemplate
 
         private void runButton_Click(object sender, EventArgs e)
         {
-            //1. Declare a string variable, put your name in it, display it
+            // 1. Declare a string variable, put your name in it, display it
 
 
-            //2. Declare an integer variable, put your age in it, display it
+            // 2. Declare an integer variable, put your age in it, display it
 
 
-            //3. Declare a double variable, put a decimal number, display it
+            // 3. Declare a double variable, put a decimal number, display it
 
 
-            //4. Combine a text with a variable to display: 
-            //   "Hello" followed by your name (e.g. Hello Mr. T)
+            /// 4. Combine a text with a variable to display: 
+            ///   "Hello" followed by your name (e.g. Hello Mr. T)
 
 
-            /* 5. Simple arithmetic is done by using the +, -, * and /, 
-               (plus, minus, times and divide). Results of mathematical
-               expressions should be stored in a variable. 
-
-               Uncomment the code below to see what the result is. Notice 
-               that the output does not show the values of x and y, 
-               but instead the words x and y. 
-
-               Change the code to subtract y from x, and output the following:
-
-               6 - 4 = 2
-            */
+            /// 5. Simple arithmetic is done by using the +, -, * and /, 
+            /// (plus, minus, times and divide). Results of mathematical
+            /// expressions should be stored in a variable. 
+            /// 
+            /// Uncomment the code below to see what the result is. Notice 
+            /// that the output does not show the values of x and y, 
+            /// but instead the words x and y. 
+            /// 
+            /// Change the code to subtract y from x, and output the following:
+            /// 
+            /// 6 - 4 = 2
 
             //int x = 6;
             //int y = 4;
@@ -57,38 +56,37 @@ namespace MathOperatorsTemplate
             //outputLabel.Text += $"\n\nx + y = {answer}";
 
 
-            /* 6. Create a program that calculates how much money an 
-                  employee has made based on their pay rate and the 
-                  number of hours worked.
+            /// 6. Create a program that calculates how much money an 
+            /// employee has made based on their pay rate and the 
+            /// number of hours worked.
+            /// 
+            /// - Create a variable to hold the rate of pay, (payRate), 
+            ///   and set it to 15.25
+            /// - Create a variable to hold the hours worked, (hoursWorked), 
+            ///   and set it to 8
+            /// - Create a variable to hold the total pay, (totalPay)
+            /// - Calculate the total pay
+            /// - Display the results to the screen as per the output below.
+            /// 
+            /// Rate of Pay: 15.25
+            /// Hours Worked: 8
+            /// Total Pay: 122
+            
 
 
-               - Create a variable to hold the rate of pay, (payRate), 
-                 and set it to 15.25
-               - Create a variable to hold the hours worked, (hoursWorked), 
-                 and set it to 8
-               - Create a variable to hold the total pay, (totalPay)
-               - Calculate the total pay
-               - Display the results to the screen as per the output below.
-
-               Rate of Pay: 15.25
-               Hours Worked: 8
-               Total Pay: 122
-            */
-
-
-            /* 7. Create a program which calculates the tax amount for a
-                  meal at a restaurant.
-                   
-              - Create a variable to hold the bill amount, (billAmount), 
-                of 22.50
-              - Create a variable to hold the tax rate, (taxRate), of 0.13
-              - Create a variable to hold the tax amount, (taxAmount)
-              - Calculate the tax, (billAmount * taxRate) and store it 
-                in taxAmount 
-
-                Bill Amount: 22.5
-                13% Tax: 2.925             
-            */
+            /// 7. Create a program which calculates the tax amount for a
+            /// meal at a restaurant.
+            /// 
+            /// - Create a variable to hold the bill amount, (billAmount), 
+            ///   of 22.50
+            /// - Create a variable to hold the tax rate, (taxRate), of 0.13
+            /// - Create a variable to hold the tax amount, (taxAmount)
+            /// - Calculate the tax, (billAmount * taxRate) and store it 
+            ///   in taxAmount 
+            ///   
+            /// Bill Amount: 22.5
+            /// 13% Tax: 2.925             
+            
 
 
             // 8. Increment your age variable by 1 using ++ and display it
